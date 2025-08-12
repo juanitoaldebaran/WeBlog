4d7344c62a3f1225ba18cb89afc84be0c37b5c6d
juanitosiahaan1711@gmail.com <juanitosiahaan1711@gmail.com>
2025-08-12 16:49:19 +0100

    add registration form

--- weblog-frontend/WeBlog/src/App.tsx
+++ weblog-frontend/WeBlog/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Router>
+       <Routes>
+        <Route path='/auth/signup'></Route>
+      </Routes>
+    </Router>
   )
 }
 
