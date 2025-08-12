caa28ef77dff74dfebb7d41e2b56cbb673f4ca48
juanitosiahaan1711@gmail.com <juanitosiahaan1711@gmail.com>
2025-08-12 21:58:56 +0100

    addition on service and api services

---
+++ weblog-frontend/WeBlog/src/services/authService.ts
@@ -0,0 +1,40 @@
+import type { LoginRequest, LoginResponse, RegisterRequest, User } from "../types/auth";
+import api from "../config/api";
+
+export async function login(credentials: LoginRequest): Promise<LoginResponse> {
+    try {
+        const response = await api.post('/auth/login', credentials);
+
+        console.log("Sending POST method for login");
+        return response.data;
+    } catch (error: any) {
+        throw new Error(error.response?.data?.message || "Failed to send login request");
+    }
+}
+
+export async function register(credentials: RegisterRequest): Promise<User> {
+    try {
+        const response = await api.post('/auth/signup', credentials);
+
+        console.log("Sending POST method for sign up");
+        return response.data;
+    } catch (error: any) {
+        throw new Error(error.response?.data?.message || "Failed to send register request");
+    }
+} 
+
+export function logout() {
+    localStorage.removeItem("jwtToken");
+    localStorage.removeItem("user");
+    window.location.href = '/auth/login';
+}
+
+export function getJwtToken(): string | null {
+    return localStorage.getItem("jwtToken");
+}
+
+export function isAuthenticated(): boolean {
+    const jwtToken = getJwtToken();
+
+    return jwtToken != null;
+}
