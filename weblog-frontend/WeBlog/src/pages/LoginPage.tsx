1cb971881fa747f7868a838847e33f2f5c17e06b
juanitosiahaan1711@gmail.com <juanitosiahaan1711@gmail.com>
2025-08-13 17:35:41 +0100

    create registration page

---
+++ weblog-frontend/WeBlog/src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "../components/auth/LoginForm"
+
+const LoginPage: React.FC = () => {
+    return (
+         <div className="min-h-screen flex items-center justify-center">
+              <LoginForm />
+        </div>
+    )
+}
+
+export default LoginPage;
