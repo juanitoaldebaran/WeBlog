bbd08f053245fd089456eda7cdc657d854a453e9
juanitosiahaan1711@gmail.com <juanitosiahaan1711@gmail.com>
2025-08-25 23:05:46 +0100

    add blog list and comments

--- weblog-frontend/WeBlog/src/components/dashboard/BlogList.tsx
+++ weblog-frontend/WeBlog/src/components/dashboard/BlogList.tsx
@@ -6,20 +6,36 @@ import Notification from "../common/Notification";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBox, faX } from "@fortawesome/free-solid-svg-icons";
 import useAuth from "../../hooks/useAuth";
+import Modal from "../common/Modal";
 
 interface BlogListProps {
     refreshTrigger: number;
 }
 
-
 const BlogList: React.FC<BlogListProps> = ({refreshTrigger}) => {
     const [blogs, setBlogs] = useState<Blog[]>([]);
     const {notification, showNotification, hideNotification} = useNotification();
+    const [showUpdateModal, setShowUpdateModal] = useState(false);
+    const [selectedBlog, setSelectedBlog] = useState<Blog>({
+        title: "",
+        category: "TECHNOLOGY",
+        content: "",
+        description: "",
+        imageUrl: "",
+    } as Blog);
     const {user} = useAuth();
+
+    const categories = [
+        "TECHNOLOGY",
+        "FINANCE",
+        "HEALTH",
+        "TRAVEL",
+        "SPORT"
+    ]
 
     const loadBlogs = async () => {
         try {
-            const allBlogs = await blogService.getAllBlogs();
+            const allBlogs = await blogService.getMyBlogs();
             showNotification("Please see all your current blogs", "success");
             setBlogs(allBlogs);
         } catch (error: any) {
@@ -28,13 +44,49 @@ const BlogList: React.FC<BlogListProps> = ({refreshTrigger}) => {
         }   
     }
 
-    const handleUpdate =  async (blogId: number) => {
-        
+    const openUpdateModal = (blog: Blog) => {
+        setSelectedBlog(blog);
+        setShowUpdateModal(true);
+    }
+
+    const handleUpdate = async () => {
+        if (!selectedBlog) return null;
+
+        try {
+            const updatedBlog = await blogService.updateBlog(selectedBlog.id, {
+                title: selectedBlog?.title,
+                content: selectedBlog?.content,
+                category: selectedBlog?.category,
+                imageUrl: selectedBlog?.imageUrl,
+                description: selectedBlog?.description,
+            });
+            setBlogs((prev) => prev.map((newBlog) => (newBlog.id === updatedBlog.id ? updatedBlog : newBlog)));
+            showNotification("New blog updated successfully", "success");
+            setShowUpdateModal(false);
+        } catch (error: any) {
+            showNotification(error?.response?.data?.message || "Failed to update current blog", "error");
+        }
     }
 
     const handleDelete = async (blogId: number) => {
-        
+        try {
+           if (window.confirm(("Do you want to delete this blog?"))) {
+                await blogService.deleteBlog(blogId);
+                setBlogs((prev) => prev.filter((blog) => blog.id != blogId));
+                showNotification("Blog deleted successfully", "success");
+            }  
+        } catch (error: any) {
+            showNotification(error?.response?.data?.message ||"Failed to delete blog","error");
+        }
     }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+        const {name, value} = e.target;
+        setSelectedBlog((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+    };
 
     useEffect(() => {
         loadBlogs();
@@ -73,7 +125,7 @@ const BlogList: React.FC<BlogListProps> = ({refreshTrigger}) => {
                                     <td className="px-4 py-2">{blog.category}</td>
                                     <td className="px-4 py-2">{blog.createdAt}</td>
                                     <td className="px-4 py-2 flex items-center justify-center gap-4">
-                                        <button onClick={() => handleUpdate(blog.id)} className="rounded text-sm border p-1 cursor-pointer">
+                                        <button onClick={() => openUpdateModal(blog)} className="rounded text-sm border p-1 cursor-pointer">
                                             Update
                                         </button>
                                         <button onClick={() => handleDelete(blog.id)} className="rounded-full text-sm bg-red-100 text-red-500 p-1 cursor-pointer">
@@ -85,7 +137,69 @@ const BlogList: React.FC<BlogListProps> = ({refreshTrigger}) => {
                         )}
                     </tbody>
                 </table>
+
+                {showUpdateModal && selectedBlog && (
+                        <Modal
+                         isOpen={showUpdateModal} 
+                         onClose={() => setShowUpdateModal(false)} 
+                         onSubmit={handleUpdate} 
+                         title="Update Blog"
+                         >
+                            <div className="p-4 flex flex-col gap-4">
+                                    <input
+                                        id="imageUrl"
+                                        name="imageUrl"
+                                        value={selectedBlog.imageUrl}
+                                        placeholder="Enter image url"
+                                        className="w-full bg-gray-100 shadow rounded p-2"
+                                        onChange={handleChange}
+                                    />
+
+                                    <input 
+                                        id="title"
+                                        name="title"
+                                        value={selectedBlog.title}
+                                        placeholder="Enter title"
+                                        className="w-full bg-gray-100 shadow rounded p-2"
+                                        onChange={handleChange}
+                                    />
+
+                                    <input 
+                                        id="content"
+                                        name="content"
+                                        value={selectedBlog.content}
+                                        placeholder="Enter content"
+                                        className="w-full bg-gray-100 shadow rounded p-2"
+                                        onChange={handleChange}
+                                    />
+
+                                    <select 
+                                        id="category"
+                                        name="category"
+                                        value={selectedBlog.category}
+                                        className="w-full bg-gray-100 shadow rounded p-2"
+                                        onChange={handleChange}
+                                    >   
+                                        {categories.map((cat) => (
+                                            <option key={cat} value={cat}>
+                                                {cat}
+                                            </option>
+                                        ))}
+                                    </select>
+
+                                    <textarea 
+                                        id="description"
+                                        name="description"
+                                        value={selectedBlog.description}
+                                        placeholder="Enter title"
+                                        className="w-full bg-gray-100 shadow rounded p-2"
+                                        onChange={handleChange}
+                                    />
+                            </div>
+                        </Modal>
+                )}
             </div>
+
             <Notification 
                 message={notification.message}
                 type={notification.type}
