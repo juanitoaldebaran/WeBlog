5d5208992706c933121c6fa6c3d282482f5e88b8
juanitosiahaan1711@gmail.com <juanitosiahaan1711@gmail.com>
2025-08-28 22:37:42 +0100

    addition on a new code

--- weblog-frontend/WeBlog/src/pages/BlogPage.tsx
+++ weblog-frontend/WeBlog/src/pages/BlogPage.tsx
@@ -137,7 +137,7 @@ const BlogPage: React.FC = () => {
         {isLoading ? (
           <div className="text-center py-16">
             <div className="inline-block animate-spin rounded-full h-12 w-12 border-4 border-blue-600 border-t-transparent"></div>
-            <p className="mt-4 text-gray-600 text-lg">Loading amazing blogs...</p>
+            <p className="mt-4 text-gray-600 text-lg">Loading blogs...</p>
           </div>
         ) : (
           <>
